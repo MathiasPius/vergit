bfec3e270220141becb1999460d020f04291cff6
Mathias Pius <contact@pius.io>
2021-04-01 11:44:53 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use clap::Clap;
+use git2::Repository;
+
+#[derive(Clap)]
+enum VersionField {
+    Major,
+    Minor,
+    Patch,
+}
+
+impl Default for VersionField {
+    fn default() -> Self {
+        VersionField::Patch
+    }
+}
+
+#[derive(Clap)]
+struct BumpCommand {
+    #[clap(subcommand)]
+    version_field: VersionField,
+}
+
+#[derive(Clap)]
+enum Commands {
+    Bump(BumpCommand),
+}
+
+#[derive(Clap)]
+struct Opts {
+    #[clap(subcommand)]
+    pub subcommand: Commands,
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let opts = Opts::parse();
+
+    let repository = Repository::open(std::env::current_dir()?)?;
+
+    let tag_names = repository.tag_names(None)?;
+
+    match &opts.subcommand {
+        Commands::Bump(bump) => match bump.version_field {
+            VersionField::Major => {
+                
+            }
+            VersionField::Minor => {}
+            VersionField::Patch => {}
+        },
+    }
+
+    Ok(())
+}
